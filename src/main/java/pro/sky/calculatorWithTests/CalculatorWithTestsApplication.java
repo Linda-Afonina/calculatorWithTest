fa2110efdbee2f88ca063b173927177f11afd3c2
linda <charming.linda85@gmail.com>
2024-06-24 19:43:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculatorWithTests/CalculatorWithTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorWithTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculatorWithTests/CalculatorWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorWithTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
